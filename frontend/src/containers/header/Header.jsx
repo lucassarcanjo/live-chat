8025d29818276aa30290ef78eb49437c82d3d377
Lucas Arcanjo <lucassarcanjo@gmail.com>
2020-12-25 12:22:46 -0300

    Bugfix: remove unused useEffect

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -1,29 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Button from "../../components/button";
 
 import Logo from "../../assets/logo.svg";
 import "./Header.scss";
 
-const Header = () => {
-  const [currentTab, setCurrentTab] = useState(0);
-
-  useEffect(() => setCurrentTab(currentTab + 1), []);
-
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <img src={Logo} alt="Logo" />
-      </div>
-      <Button label="Messages" type="button" className="header__button" />
-      <Button
-        label="Participants"
-        type="button"
-        className="header__button"
-        variant="secondary"
-      />
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header">
+    <div className="header__logo">
+      <img src={Logo} alt="Logo" />
+    </div>
+    <Button label="Messages" type="button" className="header__button" />
+    <Button
+      label="Participants"
+      type="button"
+      className="header__button"
+      variant="secondary"
+    />
+  </header>
+);
 
 export default Header;
