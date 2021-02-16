fe83d08777a2956dacfdce28aab358026ab6f0af
Lucas Arcanjo <lucassarcanjo@gmail.com>
2021-02-16 13:17:22 -0300

    Bugfix react base url

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,5 +1,5 @@
 const baseApiUrl = process.env.REACT_APP_API_URL || "https://localhost:5001";
-export const websocketHubUrl = "${/hubs/chat";
+export const websocketHubUrl = `${baseApiUrl}/hubs/chat`;
 export const sendMessageHubUrl = `${baseApiUrl}/chat/messages`;
 export const randomNameApiUrl =
   "https://random-name-generator-api.herokuapp.com/name";
